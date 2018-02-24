01e427010eeda9444ebe56cd1ff0fb2738934522
Brian Jenkins <brianjenkins94@gmail.com>
2018-02-24 16:54:20 -0500

    Updated ports

--- ubuntu14.04/run.sh
+++ ubuntu14.04/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 3000:3000 -p 8080:8080 -p 18681:18681 -p 18682:18682 --rm -it --name prizmdocker-ubuntu14.04 prizmdocker-ubuntu14.04:latest bash -c "Xvfb :20 &> /dev/null & export DISPLAY=:20 && curl -s https://raw.githubusercontent.com/brianjenkins94/PrizmDocker/master/pdutil.sh | bash -s license && bash"
+docker run -p 80:80 -p 3000:3000 -p 8080:8080 -p 18001:18001 -p 18681:18681 -p 18682:18682 --rm -it --name prizmdocker-ubuntu14.04 prizmdocker-ubuntu14.04:latest bash -c "Xvfb :20 &> /dev/null & export DISPLAY=:20 && curl -s https://raw.githubusercontent.com/brianjenkins94/PrizmDocker/master/pdutil.sh | bash -s license && bash"
