a5c7ae4edced0291e8d2de1b52382bfa03eb1b5e
Brian Jenkins <brianjenkins94@gmail.com>
2018-03-30 15:09:23 -0400

    Correcting Dockerfile syntax errors

--- centos7/run.sh
+++ centos7/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 80:80 -p 3000:3000 -p 8080:8080 -p 18001:18001 -p 18681:18681 -p 18682:18682 --rm -it --name prizmdocker-centos7 prizmdocker-centos7:latest
+docker run -p 3000:3000 -p 8080:8080 -p 18001:18001 -p 18681:18681 -p 18682:18682 --rm -it --name prizmdocker-centos7 prizmdocker-centos7:latest
